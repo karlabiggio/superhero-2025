1eeace64313cfb89c4b6c55245f3aa9dc597e96d
karlabiggio <vbkarlai@gmail.com>
2025-10-08 20:41:38 +0200

    cambio el viewModel

--- app/src/main/java/edu/iesam/superheroapi/features/superheroes/features/presentation/SuperHeroesListViewModel.kt
+++ app/src/main/java/edu/iesam/superheroapi/features/superheroes/features/presentation/SuperHeroesListViewModel.kt
@@ -3,15 +3,16 @@ package edu.iesam.superheroapi.features.superheroes.presentation
 import androidx.annotation.RestrictTo
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import edu.iesam.superheroapi.features.superheroes.data.remote.SuperHeroesApiRemoteDataSource
 import edu.iesam.superheroapi.features.superheroes.domain.FetchSuperheroesUseCase
 import edu.iesam.superheroapi.features.superheroes.domain.SuperheroRepository
 import kotlinx.coroutines.launch
 
-class SuperHeroesListViewModel(private val useCase: FetchSuperheroesUseCase): ViewModel(){
+class SuperHeroesListViewModel(private val dataSource: SuperHeroesApiRemoteDataSource): ViewModel(){
 
     fun loadSuperHeroes() {
         viewModelScope.launch {
-            useCase.invoke()
+            val superHeroes = dataSource.getSuperHeroes()
         }
     }
 
